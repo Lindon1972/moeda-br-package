cf5ae393c0a7634869d43204e464ba4575aa7a15
Lindon Macedo <66036865+Lindon1972@users.noreply.github.com>
2024-01-30 12:26:43 -0300

    Create conversor_de_moeda.py

---
+++ moeda_br/conversor_de_moeda.py
@@ -0,0 +1,8 @@
+def conversor(valor):
+    if not type(valor) == float:
+        float(valor)
+        
+    str_valor = f'R$ {valor:_.2f}'
+    str_valor = str_valor.replace('.',',').replace('_','.')
+    
+    return str_valor
